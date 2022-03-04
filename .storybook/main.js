a7a8863ea3d1b1a469ac1f03b654fa0e1415f08e
Leo Vigna <leo.vigna@gmail.com>
2022-03-04 16:18:23 +0100

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+module.exports = {
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/preset-scss',
+        {
+            name: '@storybook/preset-create-react-app',
+            options: {
+                scriptsPackageName: 'react-scripts',
+            },
+        },
+    ],
+    framework: '@storybook/react',
+    core: {
+        builder: 'webpack5'
+    },
+    /*
+    webpack: async (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+    */
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+# React Starter Template
+React template built with CRA and configured with additional utilities:
+* Github Workflows: `.github`
+* StorybookJS with Webpack5
+* Husky
+* ESLint Rules: `.eslitrc.js`
+* Editor Config: `.editorconfig`, `.vscode`
+* Netlify Config: `.netlify`
